8a903bb34e110d8c2423d19f6fd87736d76fe92a
AdiBau2019 <ap_maly@o2.pl>
2023-10-22 18:39:28 +0200

    create index.ts file

---
+++ index.ts
@@ -0,0 +1,26 @@
+import * as express from "express";
+const cookieParser = require("cookie-parser");
+import * as cors from "cors";
+const path = require("path");
+const fileUpload = require("express-fileupload");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(fileUpload({ useTempFiles: true, tempFileDir: path.join(__dirname, "temp"), uploadTimeout: 0, defParamCharset: "utf8" }));
+
+app.use(function (req, res, next) {
+	req.accepts(["json", "html", "text/plain", "application/json"]);
+	res.setHeader("Access-Control-Allow-Headers", ["Content-Type", "auth"]);
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.setHeader("Access-Control-Allow-Methods", "POST GET");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	next();
+});
+
+app.listen(3001, "0.0.0.0", () => {
+	console.log("Listening on http://localhost:3001");
+});
